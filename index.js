a458b3b1316ce7fa36352f440f79790c0121506d
Rana Ahmed <AbstractProgrammer@example.com>
2024-08-19 22:47:05 +0300

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const fs = require("fs")
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on Port ${port}`));
@@ -10,21 +11,90 @@ app.use(express.json());
 
 //get req
 app.get("/", (req, res) => {
-    res.status(200).json({ msg: "Get Request" });
+    fs.readFile("database.json", "utf8", (err, data) => {
+        if (err) {
+            res.status(400).json({ err });
+        }
+        if (data) {
+            res.status(200).json({ data: JSON.parse(data) });
+        }
+    });
+
 });
 
-//post req
 app.post("/", (req, res) => {
-    res.status(201).json({ msg: "Post Request" });
-    console.log(req.body);
+    fs.readFile("database.json", "utf8", (err, data) => {
+        if (err) {
+            return res.status(400).json({ msg: "Error reading database" });
+        }
+        let database = JSON.parse(data);
+        const lastUser = database.users.reduce((prev, current) => {
+            return (prev.id > current.id) ? prev : current;
+        });
+        const newId = lastUser.id + 1;
+        const newUser = {
+            id: newId,
+            name: req.body.name
+        };
+        database.users.push(newUser);
+        fs.writeFile("database.json", JSON.stringify(database), (err) => {
+            if (err) {
+                return res.status(400).json({ msg: "Error writing to database" });
+            }
+            console.log(req.body);
+            res.status(201).json({ msg: "User added successfully", user: newUser });
+        });
+    });
 });
 
-//put req
+// //put req
+
 app.put("/:id", (req, res) => {
-    res.status(201).json({id : req.params.id});
+    const { name } = req.body;
+    fs.readFile("database.json", "utf8", (err, data) => {
+        if (err) {
+            return res.status(400).json({ msg: "Error reading database" });
+        }
+
+        if (data) {
+            let database = JSON.parse(data);
+            const users = database["users"];
+
+            users[users.findIndex((e) => e.id == req.params.id)].name = name;
+            fs.writeFile("database.json", JSON.stringify(database), (err) => {
+                if (err) {
+                    return res.status(400).json({ msg: "Error writing to database" });
+                }
+                res.status(201).json({ msg: "User updated successfully", user: database });
+            });
+        }
+
+
+    });
 });
 
-//delete req
+// //delete req
 app.delete("/:id", (req, res) => {
-    res.status(201).json({id : req.params.id});
-});
+    const id = req.params.id;
+    fs.readFile("database.json", "utf8", (err, data) => {
+      if (err) res.status(400).json({ err });
+      if (data) {
+        let database = JSON.parse(data);
+        let users = database["users"];
+        users = users.filter((e) => e.id != id);
+  
+        fs.writeFile(
+          "database.json",
+          JSON.stringify(database),
+          (err) => {
+            if (err) {
+              res.status(400).json({ err });
+            } else {
+              res.status(201).json({ msg: "user has been deleted", user: database });
+            }
+          }
+        );
+      }
+    });
+  });
+  
