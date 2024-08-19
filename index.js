711820c9635b590bdbccb9e693e3b0e01cc994ee
Rana Ahmed <AbstractProgrammer@example.com>
2024-08-19 19:45:15 +0300

    Post Request

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on Port ${port}`));
 
+//middleware
+app.use(express.json());
+
 //get req
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Get Request" });
+});
+
+//post req
+app.get("/", (req, res) => {
+    res.status(201).json(req.body);
 });
