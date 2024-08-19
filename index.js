6f3971bd74c3d2df22f9d3537dd0ac045177f0e3
Rana Ahmed <AbstractProgrammer@example.com>
2024-08-19 19:24:21 +0300

    Get Request

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT;
-app.listen(port, () => console.log(`Server is running on Port ${port}`))
+app.listen(port, () => console.log(`Server is running on Port ${port}`));
+
+//get req
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "Get Request" });
+});
