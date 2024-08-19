5b41c3b174decfea05551b73c49356b17401548c
Rana Ahmed <AbstractProgrammer@example.com>
2024-08-19 18:53:50 +0300

    Main Branch

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server is running on Port ${port}`))
